266e5a00c7647e47c9a4a3656429a3ad9b2593c7
Glaucio Daniel <glauciodaniel@gmail.com>
2022-09-29 09:21:01 -0300

    Deploy

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+//trabalhando com JavaScript em um arquivo externo.
+const nome = "Fiap";
+console.log(nome, " chamando variável criada com var");
+nome = "Avanade";
+
+escreve("Exemplo de função nominal");
+
+//função nominal - udf
+function escreve(titulo) {
+  alert(titulo);
+}
+
+//7 tipos diferentes de erros em JavaScript - 3 são os mais comuns.
+// 1 - SyntaxError -
+// 2 - ReferenceError -
+// 3 - TypeError -
+// 4 - RangeError
+// 5 - EvalError
+// 6 - InternalError
+// 7 - URIError
