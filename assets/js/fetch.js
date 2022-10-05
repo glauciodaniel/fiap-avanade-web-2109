76819c1b37163d41aecb8cf191eab3616b142cb2
Glaucio Daniel <glauciodaniel@gmail.com>
2022-10-05 11:49:45 -0300

    Deploy

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,30 +1,46 @@
 const cep = document.querySelector("#cep");
-cep.addEventListener("blur", (e) => {
-  let search = cep.value.replace("-", "");
 
+const showData = (result) => {
+  for (const campo in result) {
+    if (document.querySelector("#" + campo)) {
+      document.querySelector("#" + campo).value = result[campo];
+    }
+  }
+};
+
+cep.addEventListener("blur", async (e) => {
+  let search = cep.value.replace("-", "");
   if (search.length < 8 || Number(search.value) === NaN) {
     alert("CEP inválido!");
     cep.focus();
     return;
   }
-
   const options = {
     method: "GET",
     mode: "cors",
     cache: "default",
   };
-
-  //recurso assíncrono, retorna uma promise por que eu não sei se vai funcionar.
-  //Promise é um recurso que não temos certeza se deu certo ou não, é uma intenção.
-  //Se der certo ele vai retornar o resolve, se der errado ele vai retornar o reject.
-  //resolver quer dizer, funcionou, aí usamos a função then, se der errado, usamos o catch.
-  fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-    .then((response) => {
-      response.json().then((result) => {
-        console.log(result);
-      });
-    })
-    .catch((err) => {
-      console.log(e.message);
-    });
+  // axios - biblioteca javascript que usa fetch e XHR.
+  try {
+    const resultado = await fetch(
+      `https://viacep.com.br/ws/${search}/json/`,
+      options
+    );
+    const json = await resultado.json();
+    showData(json);
+  } catch (error) {
+    console.log("Temos um problemas", error.message);
+  }
 });
+
+//   fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+//     .then((response) => {
+//       response.json().then((result) => {
+//         console.log(result);
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(e.message);
+//     });
+
+//outra forma de fazer o mesmo código acima é:
